1ebd57a3b4566acdc20479fb37ad3c5f2d98861b
David <dcmejiaz@geosat.com.co>
2022-04-23 08:37:07 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import DisplayUsersData from './displayUsers';
+import { Row, Col } from 'antd';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+const client = new ApolloClient({
+uri: "http://localhost:5000/graphql",
+cache: new InMemoryCache(),
+
+});
+
+return (
+<>
+ <ApolloProvider client={client}>
+    <Row justify="space-around">
+      <Col span={10}>
+          <Component {...pageProps} />
+      </Col>
+          
+      <Col span={10}>
+      <br/>
+          <DisplayUsersData/>
+      </Col>
+    </Row>
+
+    </ApolloProvider>
+
+</>
+)
 }
 
 export default MyApp
